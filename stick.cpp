7260e1b7a0adfc134c3a4e2fe9055873f30677f2
Prashant Dubey <pdubey1924@gmail.com>
2025-02-03 14:37:24 +0530

    solved

--- stick.cpp
+++ stick.cpp
@@ -1,13 +1,24 @@
 #include<bits/stdc++.h>
-using namespace std;
+
 
 // macros
 
-#define ll long long 
+#define ll  long long
+using namespace std;
+#define nline endl;
 void solve(){
     ll x,y,k;
     cin>>x>>y>>k;
+    ll ans = 0;
+    long double coal = 0;
+    long double stick = 0;
+    long double div = 0;
     
+     coal = y*k;
+     stick = coal + (k-1);
+    div = ceil(stick/(x-1));
+    ans = div + k;
+    cout<<ans<<nline; 
 }
 int main(){
     int t;
