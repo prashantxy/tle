84b0ef46544016b589819b3257681cfa8500a4e9
Prashant Dubey <pdubey1924@gmail.com>
2025-02-08 11:34:33 +0530

    updated

--- MONSTER.cpp
+++ MONSTER.cpp
@@ -12,17 +12,7 @@ void solve()
     for (int i = 0; i < n; i++)
         cin >> arr[i];
 
-    vector<pair<ll, ll>>pair_indices;  //to store th eoriginal valuue kyuki last mai original indices nahi lost hone chahiye
-    for (int i = 0; i < n; i++)
-    {
-        ll res = arr[i] % k;
-        if (res == 0)
-            res += k;
-            pair_indices.push_back({-res, i});     //here
-    }
-    sort(pair_indices.begin(), pair_indices.end());
-    for (auto el : pair_indices)
-        cout << el.second + 1 << " ";   //one - based indexing
+     
 
 }
 
