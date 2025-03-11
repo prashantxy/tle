bb1ab2d82fb14e7348f0926033e088b58a7fbc7f
Prashant Dubey <pdubey1924@gmail.com>
2025-03-11 19:59:32 +0530

    up

--- div2C10.cpp
+++ div2C10.cpp
@@ -1,41 +1,25 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-void solve() {
-    int n;
-    cin >> n;
-    vector<long long> b(2 * n);
-    
-    long long sumB = 0;
-    for (int i = 0; i < 2 * n; i++) {
-        cin >> b[i];
-        sumB += b[i];
-    }
-
-    sort(b.begin(), b.end()); 
-
-    for (int i = 0; i < 2 * n; i++) {
-        long long target = sumB - 2 * b[i];  
-        if (binary_search(b.begin(), b.begin() + i, target) || 
-            binary_search(b.begin() + i + 1, b.end(), target)) {
-            cout << target << " "; 
-            for (int j = 0; j < 2 * n; j++) {
-                if (j != i) cout << b[j] << " ";
-            }
-            cout << "\n";
-            return;
-        }
-    }
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
+int main(){
     int t;
-    cin >> t;
-    while (t--) {
-        solve();
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        n*=2;
+        vector<int> a(n);
+        vector<int> b,c;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a.rbegin(),a.rend());
+        long long last=a[0];
+        cout<<a[0]<<" ";
+        for(int i=1;i<n/2;i++){
+            cout<<a[n-i]<<" "<<a[i]<<" ";
+            last+=(a[i]-a[n-i]);
+        }
+        last+=(a[n/2]);
+        cout<<last<<" "<<a[n/2]<<endl;
     }
-    return 0;
 }
