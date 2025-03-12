80e9672d699d9fa3fe7c22cbfc7e82cf28874dcc
Prashant Dubey <pdubey1924@gmail.com>
2025-03-12 19:31:21 +0530

    up

--- div3D11.cpp
+++ div3D11.cpp
@@ -14,39 +14,48 @@ int main(){
         int n, m;
         cin >> n >> m;
         vector<ll> centers(n);
-        vector<int> radii(n);
+        vector<ll> radii(n);
+        
         for (int i = 0; i < n; i++){
             cin >> centers[i];
         }
-        int sumR = 0;
-        int maxR = 0;
+        
+        ll sumR = 0;
+        ll maxR = 0;
+        
         for (int i = 0; i < n; i++){
             cin >> radii[i]; 
             sumR += radii[i];
             maxR = max(maxR, radii[i]);
         }
-        int offset = maxR; 
+        
+        ll offset = maxR; 
         vector<vector<Interval>> rowIntervals(2 * maxR + 1); 
+        
         for (int i = 0; i < n; i++){
-            int r = radii[i];
+            ll r = radii[i];
             ll cx = centers[i];
-            for (int y = -r; y <= r; y++){
-                int dy2 = y * y;
-                int rem = r * r - dy2;
-                int d = (int) floor(sqrt(rem));
+            for (ll y = -r; y <= r; y++){
+                ll dy2 = y * y;
+                ll rem = r * r - dy2;
+                if (rem < 0) continue; 
+                ll d = (ll) floor(sqrt((double) rem));
                 ll L = cx - d;
                 ll R = cx + d;
                 rowIntervals[y + offset].push_back({L, R});
             }
         }
+        
         ll totalPoints = 0;
         for (int y = 0; y < (int) rowIntervals.size(); y++){
             if(rowIntervals[y].empty()) continue;
             sort(rowIntervals[y].begin(), rowIntervals[y].end(), [](const Interval &a, const Interval &b){
                 return a.first < b.first;
             });
+            
             Interval cur = rowIntervals[y][0];
             ll rowCount = 0;
+            
             for (size_t j = 1; j < rowIntervals[y].size(); j++){
                 Interval nxt = rowIntervals[y][j];
                 if(nxt.first <= cur.second + 1) {
@@ -58,7 +67,6 @@ int main(){
             }
             rowCount += (cur.second - cur.first + 1);
             totalPoints += rowCount;
-           // cout<<totalPoints<<" ";
         }
         
         cout << totalPoints << "\n";
